ed7d91b37a54ef52080686bc6dc176ffefad1cd9
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-12-22 18:03:56 +0900

    feat(AttendanceUpdateOutputView): 출석 출력 기능 구현

---
+++ src/main/kotlin/attendance/view/AttendanceUpdateOutputView.kt
@@ -0,0 +1,14 @@
+package attendance.view
+
+import common.AttendanceState
+import common.getDayOfWeek
+import java.time.LocalDateTime
+
+class AttendanceUpdateOutputView {
+
+    fun printUpdateResult(previousDateTime: LocalDateTime, updateTime: LocalDateTime) {
+        val previousAttendanceState = AttendanceState.convertAttendanceState(previousDateTime)
+        val updateAttendanceState = AttendanceState.convertAttendanceState(updateTime)
+        println("${previousDateTime.monthValue}월 ${previousDateTime.dayOfMonth}일 ${previousDateTime.dayOfWeek.getDayOfWeek()} ${previousDateTime.toLocalTime()} (${previousAttendanceState.result}) -> ${updateTime.toLocalTime()} (${updateAttendanceState.result}) 수정 완료!")
+    }
+}

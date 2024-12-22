2188bbd69d7089c5a24b0e00ae8db8d8885341bc
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-12-22 12:58:42 +0900

    feat(AttendanceConfirmationController): 등교 시간을 구하는 기능 구현

--- src/main/kotlin/attendance/controller/AttendanceConfirmationController.kt
+++ src/main/kotlin/attendance/controller/AttendanceConfirmationController.kt
@@ -9,12 +9,23 @@ class AttendanceConfirmationController(
     private val inputView = AttendanceConfirmationInputView()
 
     fun run() {
-        val nickName = inputView.readAttendanceName()
-        validateNickName(nickName)
+        val nickName = getNickName(inputView.readAttendanceName())
+        val arrivalTime = getArrivalTime(inputView.readArrivalTime())
+
     }
 
-    private fun validateNickName(name: String) {
+    private fun getNickName(name: String) {
         val containsCrewName = attendanceService.getCrewNames().contains(name)
         require(containsCrewName) { "[ERROR] 등록되지 않은 닉네임입니다." }
+    }
+
+    private fun getArrivalTime(time: String): Pair<Int, Int> {
+        require(time.split(":").size == 2)
+        val (hourInput, minuteInput) = time.split(":")
+        val hour = requireNotNull(hourInput.toIntOrNull()) { "[ERROR] 잘못된 형식을 입력하였습니다." }
+        val minute = requireNotNull(minuteInput.toIntOrNull()) { "[ERROR] 잘못된 형식을 입력하였습니다." }
+        require(hour in 0..23) { "[ERROR] 잘못된 형식을 입력하였습니다." }
+        require(minute in 0..59) { "[ERROR] 잘못된 형식을 입력하였습니다." }
+        return Pair(hour, minute)
     }
 }

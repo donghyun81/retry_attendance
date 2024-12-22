9b826357e26a2bd3b0d96a5b76a71d2fabde3833
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-12-22 17:35:52 +0900

    feat(AttendanceState): 출석 결과를 열거형 객체로 분리

---
+++ src/main/kotlin/common/AttendanceState.kt
@@ -0,0 +1,20 @@
+package common
+
+import java.time.DayOfWeek
+import java.time.LocalDateTime
+
+enum class AttendanceState(val result: String) {
+    ATTENDANCE("출석"),
+    PERCEPTION("지각"),
+    ABSENCE("결석");
+
+    companion object {
+        fun convertAttendanceState(date: LocalDateTime): AttendanceState {
+            if (date.dayOfWeek == DayOfWeek.MONDAY && date.hour >= 13 && date.minute > 5) return PERCEPTION
+            if (date.dayOfWeek == DayOfWeek.MONDAY && date.hour >= 13 && date.minute > 30) return ABSENCE
+            if (date.dayOfWeek.isHoliday().not() && date.hour >= 10 && date.minute > 5) return PERCEPTION
+            if (date.dayOfWeek.isHoliday().not() && date.hour >= 10 && date.minute > 30) return ABSENCE
+            return ATTENDANCE
+        }
+    }
+}

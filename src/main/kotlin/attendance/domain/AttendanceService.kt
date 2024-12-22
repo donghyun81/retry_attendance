e66745633bf6268a6f97380204fd046358c796ab
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-12-22 22:04:07 +0900

    feat(AttendanceService): 오늘까지 없는 출석 결석으로 추가하는 기능 추가

--- src/main/kotlin/attendance/domain/AttendanceService.kt
+++ src/main/kotlin/attendance/domain/AttendanceService.kt
@@ -1,6 +1,11 @@
 package attendance.domain
 
+import camp.nextstep.edu.missionutils.DateTimes
+import common.AttendanceState
+import common.getDayOfWeek
+import common.isHoliday
 import java.io.FileReader
+import java.time.LocalDate
 import java.time.LocalDateTime
 import java.time.LocalTime
 import java.time.format.DateTimeFormatter
@@ -16,10 +21,25 @@ class AttendanceService(
 
     private fun initCrewAttendance() {
         crewAttendances = FileReader("src/main/resources/attendances.csv").readLines().drop(1).map {
-            val (nickName, dateTime) = it.split(",")
+            val (nickName, dateTimeInput) = it.split(",")
             val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm")
-            CrewAttendance(nickName, LocalDateTime.parse(dateTime, formatter))
+            val dateTime = LocalDateTime.parse(dateTimeInput, formatter)
+            val attendanceState = AttendanceState.convertAttendanceState(dateTime)
+            CrewAttendance(nickName, dateTime, attendanceState)
         }.toMutableList()
+        val today = DateTimes.now()
+        for (day in 1..<today.dayOfMonth) addAbsence(getCrewNames(), today, day)
+    }
+
+    private fun addAbsence(crews: List<String>, today: LocalDateTime, day: Int) {
+        crews.forEach { name ->
+            val crewAttendance = crewAttendances.find { it.nickname == name && it.datetime.dayOfMonth == day }
+            val currentDate = LocalDate.of(today.year, today.monthValue, day)
+            if (crewAttendance == null && currentDate.dayOfWeek.isHoliday().not()) {
+                val noTime = LocalTime.of(23, 59)
+                addAttendance(name, LocalDateTime.of(currentDate, noTime))
+            }
+        }
     }
 
     private fun getCrewNames() = crewAttendances.map { it.nickname }
@@ -39,15 +59,20 @@ class AttendanceService(
     }
 
     fun addAttendance(name: String, dateTime: LocalDateTime) {
-        crewAttendances.add(CrewAttendance(name, dateTime))
+        val attendanceState = AttendanceState.convertAttendanceState(dateTime)
+        crewAttendances.add(CrewAttendance(name, dateTime, attendanceState))
     }
 
     fun updateAttendance(name: String, day: Int, updateTime: LocalTime): CrewAttendance {
         val attendanceIndex = crewAttendances.indexOfFirst { it.nickname == name && day == it.datetime.dayOfMonth }
         require(attendanceIndex != -1)
         val currentAttendance = crewAttendances[attendanceIndex]
-        val updateDateTime = currentAttendance.datetime.also { LocalDateTime.of(it.toLocalDate(), updateTime) }
-        crewAttendances[attendanceIndex] = CrewAttendance(name, updateDateTime)
+        val updateDateTime = currentAttendance.datetime.let { LocalDateTime.of(it.toLocalDate(), updateTime) }
+        val attendanceState = AttendanceState.convertAttendanceState(updateDateTime)
+        crewAttendances[attendanceIndex] = CrewAttendance(name, updateDateTime, attendanceState)
         return currentAttendance
     }
+
+    fun getCrewAttendances(name: String) =
+        crewAttendances.filter { it.nickname == name }.sortedBy { it.datetime.dayOfMonth }
 }

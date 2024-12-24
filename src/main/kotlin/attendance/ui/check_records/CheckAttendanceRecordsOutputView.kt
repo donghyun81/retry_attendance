cc4056552a1df455f3b4cd5a735f21d8c2d9b920
윤동현 <82762769+donghyun81@users.noreply.github.com>
2024-12-24 14:25:28 +0900

    refactor: 열거형 객체를 활용해서 출력하도록 수정

--- src/main/kotlin/attendance/ui/check_records/CheckAttendanceRecordsOutputView.kt
+++ src/main/kotlin/attendance/ui/check_records/CheckAttendanceRecordsOutputView.kt
@@ -2,6 +2,7 @@ package attendance.ui.check_records
 
 import attendance.domain.entity.CrewAttendance
 import common.AttendanceState
+import common.CrewAttendanceState
 import common.formatTwoDigit
 import common.getDayOfWeek
 
@@ -10,7 +11,7 @@ class CheckAttendanceRecordsOutputView {
     fun printCrewAttendances(attendances: List<CrewAttendance>) {
         println("이번 달 ${attendances.first().nickname}의 출석 기록입니다.\n")
         attendances.forEach { attendance ->
-            if (attendance.datetime.hour == 23 && attendance.datetime.minute ==59) println("${attendance.datetime.monthValue}월 ${attendance.datetime.dayOfMonth}일 ${attendance.datetime.dayOfWeek.getDayOfWeek()} --:-- (${attendance.attendanceState.result})")
+            if (attendance.datetime.hour == 23 && attendance.datetime.minute == 59) println("${attendance.datetime.monthValue}월 ${attendance.datetime.dayOfMonth}일 ${attendance.datetime.dayOfWeek.getDayOfWeek()} --:-- (${attendance.attendanceState.result})")
             else println("${attendance.datetime.monthValue}월 ${attendance.datetime.dayOfMonth.formatTwoDigit()}일 ${attendance.datetime.dayOfWeek.getDayOfWeek()} ${attendance.datetime.toLocalTime()} (${attendance.attendanceState.result})")
         }
         printState(attendances)
@@ -21,7 +22,8 @@ class CheckAttendanceRecordsOutputView {
         val perceptionCount = attendances.count { it.attendanceState == AttendanceState.PERCEPTION }
         val absenceCount = attendances.count { it.attendanceState == AttendanceState.ABSENCE }
         printAttendancesState(attendanceCount, perceptionCount, absenceCount)
-        printCrewState(perceptionCount, absenceCount)
+        val crewAttendanceState = CrewAttendanceState.getCrewAttendanceState(perceptionCount, absenceCount)
+        printCrewState(crewAttendanceState)
     }
 
     private fun printAttendancesState(attendanceCount: Int, perceptionCount: Int, absenceCount: Int) {
@@ -30,11 +32,12 @@ class CheckAttendanceRecordsOutputView {
         println("결석: ${absenceCount}회\n")
     }
 
-    private fun printCrewState(perceptionCount: Int, absenceCount: Int) {
-        when {
-            perceptionCount.div(3) + absenceCount >= 2 -> println("경고 대상자입니다.")
-            perceptionCount.div(3) + absenceCount >= 3 -> println("면담 대상자입니다.")
-            perceptionCount.div(3) + absenceCount >= 5 -> println("제적 대상자입니다.")
+    private fun printCrewState(crewAttendanceState: CrewAttendanceState) {
+        when (crewAttendanceState) {
+            CrewAttendanceState.EXPULSION -> println("${crewAttendanceState.state} 대상자입니다.")
+            CrewAttendanceState.INTERVIEW -> println("${crewAttendanceState.state} 대상자입니다.")
+            CrewAttendanceState.WARNING -> println("${crewAttendanceState.state} 대상자입니다.")
+            CrewAttendanceState.NON -> Unit
         }
     }
 }
--- src/main/kotlin/attendance/ui/check_risk_expulsion/CheckRiskOfExpulsionOutputView.kt
+++ src/main/kotlin/attendance/ui/check_risk_expulsion/CheckRiskOfExpulsionOutputView.kt
@@ -2,6 +2,7 @@ package attendance.ui.check_risk_expulsion
 
 import attendance.domain.entity.CrewAttendance
 import common.AttendanceState
+import common.CrewAttendanceState
 
 class CheckRiskOfExpulsionOutputView {
     fun printCrewsAtRisk(sortedCrewNames: List<String>, crewAttendances: List<CrewAttendance>) {
@@ -15,16 +16,9 @@ class CheckRiskOfExpulsionOutputView {
                 crewAttendances.count { crewName == it.nickname && it.attendanceState == AttendanceState.PERCEPTION }
             val absenceCount =
                 crewAttendances.count { crewName == it.nickname && it.attendanceState == AttendanceState.ABSENCE }
-            val crewState = getCrewState(perceptionCount, absenceCount)
-            if (crewState.isEmpty()) continue
-            println("- ${crewName}: 결석 ${absenceCount}회, 지각 ${perceptionCount}회 (${crewState})\"")
+            val crewState = CrewAttendanceState.getCrewAttendanceState(perceptionCount, absenceCount)
+            if (crewState == CrewAttendanceState.NON) continue
+            println("- ${crewName}: 결석 ${absenceCount}회, 지각 ${perceptionCount}회 (${crewState.state})\"")
         }
-    }
-
-    private fun getCrewState(perceptionCount: Int, absenceCount: Int): String {
-        if (perceptionCount.div(3) + absenceCount >= 5) return "제적"
-        if (perceptionCount.div(3) + absenceCount >= 3) return "면담"
-        if (perceptionCount.div(3) + absenceCount >= 2) return "경고"
-        return ""
     }
 }
